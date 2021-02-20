286fb5dee9ddfb796b84d805e59867633833dd23
imran <imufun2@gmail.com>
2021-02-20 09:24:36 +0600

    new better jump add

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -14,14 +14,16 @@ public class Player : MonoBehaviour
     //[SerializeField]
     //private float _speed = 0.5f;
 
-    [SerializeField]
-    private bool _grounded = false;
+    //[SerializeField]
+    //private bool _grounded = false;
 
     [SerializeField]
     private float _jumpForced = 5f;
 
     [SerializeField]
     private LayerMask _groundLayerMask;
+
+    private bool resetJumpNeeded = false;
 
     Animator anim;
     void Start()
@@ -36,15 +38,29 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+        movement();
+
+
+
+
+    }
+
+    void movement()
+    {
         transalation = Input.GetAxis("Horizontal");
+        _rigid.velocity = new Vector2(transalation, _rigid.velocity.y); // left & right movement
 
-
-        if(Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded() == true) // jump
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForced);
-            _grounded = false;
+
+            StartCoroutine(ResetJummpedRoutine());
         }
 
+    }
+
+    bool isGrounded()
+    {
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 5f, _groundLayerMask);
         Debug.DrawRay(transform.position, Vector2.down * 0.5f, Color.green);
@@ -52,10 +68,21 @@ public class Player : MonoBehaviour
 
         if (hitInfo.collider != null)
         {
-            _grounded = true;
+
+            //return resetJumpNeeded = true;
+            if (resetJumpNeeded == false)
+                return true;
         }
 
+      return  false;
 
-        _rigid.velocity = new Vector2(transalation, _rigid.velocity.y); 
+    }
+
+    IEnumerator ResetJummpedRoutine()
+    {
+        resetJumpNeeded = true;
+        yield return new WaitForSeconds(0.1f);
+
+        resetJumpNeeded = false;
     }
 }
